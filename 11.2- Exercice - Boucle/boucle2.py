8bde61ac3815e01de4189163a3226b96a30a388c
Faton Murtezi <faton.murtezi@hes-so.ch>
2019-09-29 21:05:45 +0200

    Version 2

--- 11.2- Exercice - Boucle/boucle2.py
+++ 11.2- Exercice - Boucle/boucle2.py
@@ -5,13 +5,7 @@
 B = [2, 6, 8, 5, 4, 12, 98, 34, 1]
 
 cpt = 0
-for i in range:
-    for j in range (1,len(B)):
+for i in range(1, len(B) ):
+    for j in range(1, len(B)):
         if B[i] > B[j]:
-            print()
-
-
-
-
-
-
+            print(B[i])
